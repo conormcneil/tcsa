c814a0f95af3d03d903d8fcbcf4061631377490e
Conor Kingston <cmkingston21@me.com>
2020-01-29 13:02:28 -0700

    app runs as module

--- app.py
+++ app.py
@@ -33,3 +33,8 @@ def upload():
             make_workbook(uploader.get_fullpath()),
             mimetype="application/vnd.ms-excel"
         )
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
